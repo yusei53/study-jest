391c052df7912f31d83d9566cde44ad984fd5ffa
yusei53 <yusei530922@gmail.com>
2023-10-09 17:42:54 +0900

    jest.fn()を使ってみた

--- src/app/test/dist/sum.test.dev.js
+++ src/app/test/dist/sum.test.dev.js
@@ -150,3 +150,15 @@ test("the data is peanut butter", function () {
 }); // test("the fetch fails with an error", () => {
 //   return expect(fetchData()).rejects.toMatch("error");
 // });
+
+test("jest.fn()のサンプル", function () {
+  var mockFn = jest.fn();
+  mockFn();
+  mockFn(1, 2, 3); // mockFnが2回呼ばれていること
+
+  expect(mockFn).toHaveBeenCalledTimes(2); // mockFnの引数に1, 2, 3が渡されていること
+
+  expect(mockFn).toHaveBeenCalledWith(1, 2, 3); // mockFnの引数に1, 2, 3, 4が渡されていないこと
+
+  expect(mockFn).not.toHaveBeenCalledWith(1, 2, 3, 4);
+});
--- src/app/test/sum.test.js
+++ src/app/test/sum.test.js
@@ -108,3 +108,18 @@ test("the data is peanut butter", () => {
 // test("the fetch fails with an error", () => {
 //   return expect(fetchData()).rejects.toMatch("error");
 // });
+
+test("jest.fn()のサンプル", () => {
+  const mockFn = jest.fn();
+  mockFn();
+  mockFn(1, 2, 3);
+
+  // mockFnが2回呼ばれていること
+  expect(mockFn).toHaveBeenCalledTimes(2);
+
+  // mockFnの引数に1, 2, 3が渡されていること
+  expect(mockFn).toHaveBeenCalledWith(1, 2, 3);
+
+  // mockFnの引数に1, 2, 3, 4が渡されていないこと
+  expect(mockFn).not.toHaveBeenCalledWith(1, 2, 3, 4);
+});
