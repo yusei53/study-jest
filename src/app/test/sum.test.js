945d483014e233a1e5aeb0ee772ca423369e5857
yusei53 <yusei530922@gmail.com>
2023-10-09 12:52:41 +0900

    配列と反復可能なオブジェクトのテスト

--- src/app/test/sum.test.js
+++ src/app/test/sum.test.js
@@ -51,3 +51,16 @@ test("there is no I in team", () => {
 test('but there is a "stop" in Christoph', () => {
   expect("Christoph").toMatch(/stop/);
 });
+
+const shoppingList = [
+  "diapers",
+  "kleenex",
+  "trash bags",
+  "paper towels",
+  "milk",
+];
+
+test("the shopping list has milk on it", () => {
+  expect(shoppingList).toContain("milk");
+  expect(new Set(shoppingList)).toContain("milk");
+});
